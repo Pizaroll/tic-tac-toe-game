154509029664cb27c17e70d0abc89d2ed9a1e1a7
pizaroll <brittanyrpizarro@gmail.com>
2024-06-03 15:43:17 -0500

    Edited Board component to declare state variable; Square component recceives letter prop from Board

--- src/App.js
+++ src/App.js
@@ -1,38 +1,32 @@
 import { useState } from 'react';
 
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
     return (
       <>
         <div className="board-row">
-          <Square />
-          <Square />
-          <Square />
+          <Square letter={squares[0]} />
+          <Square letter={squares[1]} />
+          <Square letter={squares[2]} />
         </div>
         <div className="board-row">
-          <Square />
-          <Square />
-          <Square />    
+          <Square letter={squares[3]} />
+          <Square letter={squares[4]} />
+          <Square letter={squares[5]} />
         </div>
         <div className="board-row">
-          <Square />
-          <Square />
-          <Square />
+          <Square letter={squares[6]} />
+          <Square letter={squares[7]} />
+          <Square letter={squares[8]} />
         </div>
       </>
     );
   }
 
-function Square() {
-  const [letter, setLetter] = useState(null);
-
-  function handleClick() {
-    setLetter('X');
-  }
-  
+function Square({letter}) {
   return ( // Returning the HTML
     <button // This is a button
       className="square" // HTML Class
-      onClick={handleClick} // HTML OnClick Functionality
     >
       {letter} 
     </button> // End button
